1a78271a72c06d5dd0eaeedeb1107f13f60e7b77
Anh Tuan Jnr <cnatuan.gdsciu@gmail.com>
2025-03-19 19:58:11 +0700

    Body Tuan

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
 import { RouterLink, RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
 import "/node_modules/primeflex/primeflex.css";
+import Body from "./components/Body.vue";
 </script>
 
 <template>
   <div class="w-screen">
     <NavBar></NavBar>
+  </div>
+  <div class="w-screen">
+
+    <Body></Body>
   </div>
 
   <RouterView />
