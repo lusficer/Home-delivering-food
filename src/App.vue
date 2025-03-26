bc31b80daa4567e6751177ee33ba2500f8ef863e
Anh Tuan Jnr <cnatuan.gdsciu@gmail.com>
2025-03-26 14:35:01 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
-<script >
-import { RouterLink, RouterView } from "vue-router";
-import "/node_modules/primeflex/primeflex.css";
-</script>
-
 <template>
-   <div>
+  <div>
     <HeroSection />
     <ReservationForm />
   </div>
   <RouterView />
 </template>
 <script>
-import HeroSection from "./components/HeroSection.vue";  // Phần giới thiệu trang
-import ReservationForm from "./components/ReservationForm.vue";  // Form đặt bàn
-
+// import HeroSection from "./components/HeroSection.vue";  // Phần giới thiệu trang
+// import ReservationForm from "./views/ReservationForm.vue";  // Form đặt bàn
+import { RouterLink, RouterView } from "vue-router";
+import "/node_modules/primeflex/primeflex.css";
 export default {
   components: {
-    HeroSection,
-    ReservationForm,
+    // HeroSection,
+    // ReservationForm,
   },
 };
 </script>
