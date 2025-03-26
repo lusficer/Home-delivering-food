60812d117355e8d77f316c8b237d74cb8f698191
hoang <bachhuy410@gmail.com>
2025-03-26 14:29:05 +0700

    fix conflict

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,8 @@ import "/node_modules/primeflex/primeflex.css";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="w-screen">
+    <RouterView />
+  </div>
 </template>
 <style></style>
